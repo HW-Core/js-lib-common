3d5788f06fc2ffef6bf2986ab5c3c121569e0900
Yehonal <hw.2@hotmail.it>
2014-11-24 03:21:51 +0100

    Restructured common library, moved code for browser under specific repository test will be the next step

--- Event.js
+++ Event.js
@@ -2,45 +2,60 @@
  * Copyright (C) 2007 - 2014 Hyperweb2 All rights reserved.
  * GNU General Public License version 3; see www.hyperweb2.com/terms/
  */
-var Hw2 = Hw2 || {}; // namespace
+define([
+    HW2PATH_JS_LIB + "common/Array.js"
+], function () {
+    var $ = Hw2Core;
+    return Hw2Core.Event = $.Class({members: [
+            {
+                attributes: "private static",
+                name: "_instances",
+                val: new Array()
+            },
+            {
+                attributes: "public",
+                name: "__constructor",
+                val: function () {
+                    this.triggers = [];
+                }
+            },
+            {
+                attributes: "public",
+                name: "bind",
+                val: function (obj) {
+                    this.triggers.push(obj);
+                }
+            },
+            {
+                attributes: "public",
+                name: "unbind",
+                val: function (obj) {
+                    Hw2Core.Array.remove(this.triggers, obj);
+                }
+            },
+            {
+                attributes: "public",
+                name: "trigger",
+                val: function (func, data) {
+                    for (index = 0; index < this.triggers.length; ++index) {
+                        var f = this.triggers[index][func];
+                        if (typeof f === 'function')
+                            var bFunc = f.apply(null, this.triggers[index], data);
+                        bFunc(); // launch bounded function
+                    }
+                }
+            },
+            {
+                attributes: "public static",
+                name: "I",
+                val: function (key) {
+                    if (typeof this.__st._instances[key] === "undefined") {
+                        this.__st._instances[key] = new Hw2Core.Event();
+                    }
 
-Hw2.Event = (function() {
-    var pub = _Event.prototype;
-    var pub_st = _Event;
-
-    var instances = [];
-
-    //CONSTRUCTOR
-    function _Event() {
-        this.triggers = [];
-    }
-
-    pub.bind = function(obj) {
-        this.triggers.push(obj);
-    };
-
-    pub.unbind = function(obj) {
-        Hw2.Array.remove(this.triggers, obj);
-    };
-
-    pub.trigger = function(func, data) {
-        for (index = 0; index < this.triggers.length; ++index) {
-            var f = this.triggers[index][func];
-            if (typeof f === 'function')
-                var bFunc = f.bind(this.triggers[index], data);
-            bFunc(); // launch bounded function
-        }
-    };
-
-    pub_st.getInstance = function(key) {
-        if (instances[key] === undefined) {
-            instances[key] = new Hw2.Event();
-        }
-
-        return instances[key];
-    };
-
-    return _Event;
-})();
-
-//# sourceURL=share/event.js
+                    return this.__st._instances[key];
+                }
+            }
+        ]}
+    );
+});
