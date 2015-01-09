219a229c43c8566cf9ac62d055e46da751a329cf
Yehonal <hw.2@hotmail.it>
2015-01-09 15:14:57 +0100

    Updated

--- testList.js
+++ testList.js
@@ -9,20 +9,14 @@ hw2.define(function () {
     return {
         dep: [
             $.const.PATH_JS_LIB + "common/Array.js",
-            $.const.PATH_JS_LIB + "common/Event.js",
             $.const.PATH_JS_LIB + "common/Object.js",
-            $.const.PATH_JS_LIB + "common/Path.js",
             $.const.PATH_JS_LIB + "common/String.js",
-            $.const.PATH_JS_LIB + "common/Uri.js",
             $.const.PATH_JS_LIB + "common/Var.js"
         ],
         test: [
-            $.const.PATH_JS_LIB + "common/tests/tests/array.js",
             $.const.PATH_JS_LIB + "common/tests/tests/event.js",
             $.const.PATH_JS_LIB + "common/tests/tests/object.js",
-            $.const.PATH_JS_LIB + "common/tests/tests/path.js",
             $.const.PATH_JS_LIB + "common/tests/tests/string.js",
-            $.const.PATH_JS_LIB + "common/tests/tests/uri.js",
             $.const.PATH_JS_LIB + "common/tests/tests/var.js"
         ]
     };
